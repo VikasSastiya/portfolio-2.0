301864ea2143b2560f29ef744df26452091d72cf
kenilgamer <kenilk677@gmail.com>
2025-06-19 13:28:23 +0530

    update viewport meta tag, add Open Graph properties, and enable sourcemaps in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
-
+  build: {
+    sourcemap: true,
+  },
 })
